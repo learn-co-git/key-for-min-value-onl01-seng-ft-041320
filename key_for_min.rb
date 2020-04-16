32b0792d117f544ff216c6e2bc0b7228ef4e7481
Neil Empey <argus.two.2@gmail.com>
2020-04-16 17:07:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash. do |key, value|
+  new_array = name_hash.collect do |key, value|
+  value
+end
+holder = new_array[0]
+ new_array.each {|ele| holder = ele if ele < holder}
+ return holder
 end
