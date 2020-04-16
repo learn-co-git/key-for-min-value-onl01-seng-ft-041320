e4a365cdac78ae0de36aeac141e02b22a6cb2417
Neil Empey <argus.two.2@gmail.com>
2020-04-16 17:08:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,6 @@ def key_for_min_value(name_hash)
 end
 holder = new_array[0]
  new_array.each {|ele| holder = ele if ele < holder}
- return holder
  for(key in name_hash)
  if(name_hash[key] == holder)
    return key 
